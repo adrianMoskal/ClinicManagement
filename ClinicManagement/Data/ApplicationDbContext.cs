6d3ac3417464b4782602167d7641425eeee834cf
adrianMoskal <adrian.moskal@icloud.com>
2022-01-27 15:58:43 +0100

    Add doctor and patient roles
    Add doctor and patient roles to dummy data

--- ClinicManagement/Data/ApplicationDbContext.cs
+++ ClinicManagement/Data/ApplicationDbContext.cs
@@ -49,31 +49,48 @@ namespace ClinicManagement.Data
 
             builder.Entity<User>()
                 .HasData(new User 
-                    { 
-                      Id = "f2430875-6f50-48b6-97e6-f9c475b602c2",
-                      FirstName = "Adrian",
-                      LastName = "Moskal",
-                      UserName = "adrianMoskal",
-                      NormalizedUserName = "ADRIANMOSKAL",
-                      EmailConfirmed = false,
-                      PasswordHash = "AQAAAAEAACcQAAAAEFHDs9VsKcDafKl4e7SJYYQpwX/kBoa0wrLRtG9i1aEY92YaINBYp/vN2kwORdKA5w==", // Password = Admin1@
-                      SecurityStamp = "JTERJVFIKFMJUFEDTFJFHEAJHLKXMBQD",
-                      ConcurrencyStamp = "003c3d02-b420-44f2-960c-02df61764c7a",
-                      PhoneNumberConfirmed = false,
-                      TwoFactorEnabled = false,
-                      LockoutEnabled = false,
-                      AccessFailedCount = 0
+                { 
+                    Id = "f2430875-6f50-48b6-97e6-f9c475b602c2",
+                    FirstName = "Adrian",
+                    LastName = "Moskal",
+                    UserName = "adrianMoskal",
+                    NormalizedUserName = "ADRIANMOSKAL",
+                    EmailConfirmed = false,
+                    PasswordHash = "AQAAAAEAACcQAAAAEFHDs9VsKcDafKl4e7SJYYQpwX/kBoa0wrLRtG9i1aEY92YaINBYp/vN2kwORdKA5w==", // Password = Admin1@
+                    SecurityStamp = "JTERJVFIKFMJUFEDTFJFHEAJHLKXMBQD",
+                    ConcurrencyStamp = "003c3d02-b420-44f2-960c-02df61764c7a",
+                    PhoneNumberConfirmed = false,
+                    TwoFactorEnabled = false,
+                    LockoutEnabled = false,
+                    AccessFailedCount = 0
                 });
 
             builder.Entity<Role>()
-                .HasData(new Role
+                .HasData(
+                    new Role
                     {
                         Id = "12c114d1-0ad1-4e02-bb85-ec6a2bceed1b",
                         Description = "All privileges",
                         Name = "Administrator",
                         NormalizedName = "ADMINISTRATOR",
                         ConcurrencyStamp = "accb09ec-6f9f-4c73-a066-f4764d88b278"
-                });
+                    },
+                    new Role
+                    {
+                        Id = "526fa4c0-f544-4ad0-9d31-0185533cdf48",
+                        Description = "Person who is giving medical care",
+                        Name = "Doctor",
+                        NormalizedName = "DOCTOR",
+                        ConcurrencyStamp = "65c225e1-5e1e-48fa-b8ab-9dd8794b9043"
+                    },
+                    new Role
+                    {
+                        Id = "d428231e-4f51-4e41-913e-056f91346c16",
+                        Description = "Person who is receiving medical care",
+                        Name = "Patient",
+                        NormalizedName = "PATIENT",
+                        ConcurrencyStamp = "59b3eba1-eaac-442f-9ca1-a2b5a3a8e287"
+                    });
 
             builder.Entity<UserRole>()
                 .HasData(new UserRole
---
+++ ClinicManagement/Data/Migrations/20220127145710_AddDoctorAndPatientRolesToDummyData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClinicManagement.Data.Migrations
+{
+    public partial class AddDoctorAndPatientRolesToDummyData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
+                values: new object[] { "526fa4c0-f544-4ad0-9d31-0185533cdf48", "65c225e1-5e1e-48fa-b8ab-9dd8794b9043", "Person who is giving medical care", "Doctor", "DOCTOR" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
+                values: new object[] { "d428231e-4f51-4e41-913e-056f91346c16", "59b3eba1-eaac-442f-9ca1-a2b5a3a8e287", "Person who is receiving medical care", "Patient", "PATIENT" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "526fa4c0-f544-4ad0-9d31-0185533cdf48");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: "d428231e-4f51-4e41-913e-056f91346c16");
+        }
+    }
+}
