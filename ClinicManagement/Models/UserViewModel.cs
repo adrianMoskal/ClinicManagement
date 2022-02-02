16ca37e50bee996dd1b13a2bbacd16cd47194c96
adrianMoskal <adrian.moskal@icloud.com>
2022-02-02 15:58:00 +0100

    Add style to admin panel, manage users view, and edit user view

--- ClinicManagement/Models/UserViewModel.cs
+++ ClinicManagement/Models/UserViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,7 +9,10 @@ namespace ClinicManagement.Models
 {
     public class UserViewModel
     {
+        [HiddenInput]
         public string UserId { get; set; }
+
+        public string UserName { get; set; }
 
         [DisplayName("First name")]
         public string FirstName { get; set; }
@@ -17,8 +21,9 @@ namespace ClinicManagement.Models
         public string LastName { get; set; }
 
         [DisplayName("Role")]
-        public string UserRole { get; set; }
+        public string Role { get; set; }
 
-        public string UserName { get; set; }
+        [DisplayName("Phone number")]
+        public string PhoneNumber { get; set; }
     }
 }
